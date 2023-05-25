5361ba2e0c662126003cf717a1f05df867dd1b29
ilkor4 <kornelyuck.ilya@yandex.ru>
2023-05-26 00:13:45 +0300

    fix: Исправить env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 const UnathorizedError = require('../errors/unauthorized-err');
 
 module.exports = (req, res, next) => {
@@ -7,7 +9,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(req.cookies.jwt, 'some-secret-key');
+    payload = jwt.verify(req.cookies.jwt, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
     return next(new UnathorizedError('Ошибка авторизации.'));
   }
